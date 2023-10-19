94b6940a240b9a2aaf6ea5a0cccf8f4a13355925
Md.Solaiman <md.solaiman366882@gmail.com>
2023-10-19 17:59:17 +0600

    login and register page design completed

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+        <section>
+            <div className=" max-w-screen-xl mx-auto px-5" >
+                <div className="py-10">
+                    <div className="title-area">
+                        <h4>Please ,</h4>
+                        <h2>Log<span>In</span></h2>
+                    </div>
+                    <div className="form-area">
+                        <form className="flex flex-col gap-5">
+                            <div className="single-input">
+                                <input type="email" name="email" placeholder="Enter your email" />
+                            </div>
+                            <div className="single-input">
+                                <input type="password" name="password" placeholder="Enter your password" />
+                            </div>
+                            <div className="submit-btn-area">
+                                <input className="c-btn w-full" type="submit" value="Login" />
+
+                                <div className="ask-to-go">
+                                    <p>Don not have account, please <Link to='/register'><span>Register</span></Link></p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+
+const Register = () => {
+    return (
+        <section>
+        <div className=" max-w-screen-xl mx-auto px-5" >
+            <div className="py-10">
+                <div className="title-area">
+                    <h4>Please ,</h4>
+                    <h2>Regist<span>er</span></h2>
+                </div>
+                <div className="form-area">
+                    <form className="flex flex-col gap-5">
+                        <div className="single-input">
+                            <input type="email" name="email" placeholder="Enter your email" />
+                        </div>
+                        <div className="single-input">
+                            <input type="password" name="password" placeholder="Enter your password" />
+                        </div>
+                        <div className="submit-btn-area">
+                            <input className="c-btn w-full" type="submit" value="Register" />
+
+                            <div className="ask-to-go">
+                                    <p>Don not have account, please <Link to='/login'><span>Login</span></Link></p>
+                                </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    );
+};
+
+export default Register;
